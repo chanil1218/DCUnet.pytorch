23ebeb62f21b495bc64807f7dd80eb609ebecbed
Chanil <chanil1218@gmail.com>
2019-02-20 18:06:47 +0900

    Add download option for sedata
    Only download test-set by default, 'all' args for download with
    train-set.

--- download_sedata.sh
+++ download_sedata.sh
@@ -4,13 +4,19 @@
 mkdir -p dataset_zip
 pushd dataset_zip
 # TRAINING DATASET
-if [ ! -f clean_trainset_28spk_wav.zip ]; then
-    wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2791/clean_trainset_28spk_wav.zip
-fi
-if [ ! -f noisy_trainset_28spk_wav.zip ]; then
-    wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2791/noisy_trainset_28spk_wav.zip
+if [[ $1 == all ]]; then
+    echo "[INFO] Downloading train dataset"
+    if [ ! -f clean_trainset_28spk_wav.zip ]; then
+        wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2791/clean_trainset_28spk_wav.zip
+    fi
+    if [ ! -f noisy_trainset_28spk_wav.zip ]; then
+        wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2791/noisy_trainset_28spk_wav.zip
+    fi
+else
+    echo "[INFO] Pass downloading train dataset, use 'all' argument for down all"
 fi
 # VALIDATION DATASET
+echo "[INFO] Downloading valid dataset"
 if [ ! -f clean_testset_wav.zip ]; then
     wget https://datashare.is.ed.ac.uk/bitstream/handle/10283/2791/clean_testset_wav.zip
 fi
@@ -22,15 +28,23 @@ popd
 ## INFLATE DATA
 mkdir -p dataset_tmp
 pushd dataset_tmp
-unzip -q -j ../dataset_zip/clean_trainset_28spk_wav.zip -d trainset_clean
-unzip -q -j ../dataset_zip/noisy_trainset_28spk_wav.zip -d trainset_noisy
+if [[ $1 == all ]]; then
+    echo "[INFO] Unzip train dataset"
+    unzip -q -j ../dataset_zip/clean_trainset_28spk_wav.zip -d trainset_clean
+    unzip -q -j ../dataset_zip/noisy_trainset_28spk_wav.zip -d trainset_noisy
+fi
+echo "[INFO] Unzip valid dataset"
 unzip -q -j ../dataset_zip/clean_testset_wav.zip -d valset_clean
 unzip -q -j ../dataset_zip/noisy_testset_wav.zip -d valset_noisy
 popd
 
 ## RESAMPLE
-declare -a arr=("trainset_clean" "trainset_noisy" "valset_clean" "valset_noisy")
-
+if [[ $1 == all ]]; then
+    declare -a arr=("trainset_clean" "trainset_noisy" "valset_clean" "valset_noisy")
+else
+    declare -a arr=("valset_clean" "valset_noisy")
+fi
+echo "[INFO] Resampling datasets: ${arr[*]}"
 mkdir -p dataset
 pushd dataset_tmp
 for d in */; do
